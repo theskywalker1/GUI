2a4ee1338099134cf897366c6ee8827d2bc2d1f8
Luke <lucas.j.powers@ou.edu>
2020-11-18 16:34:26 -0600

    Made buttons

--- GUI.java
+++ GUI.java
@@ -1,9 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class GUI {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final int HEIGHT = 50;
+	private static final int WIDTH = 850;
 
+	public static void main(String[] args) {
+		
+		JFrame hi = new JFrame("Darkness then redness then whiteness...");
+		hi.setSize(HEIGHT, WIDTH);
+		hi.setLocationRelativeTo(null);
+		hi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FlowLayout excuses = new FlowLayout();
+		hi.setLayout(excuses);
+		
+		JButton initialButt = new JButton("Niki is cute");
+		JButton secondButt = new JButton("Spongeboy me bob");
+		JButton bensButt = new JButton("Push the BUTTon");
+
+		hi.add(initialButt);
+		hi.add(secondButt);
+		hi.add(bensButt);
+		
+		hi.pack();
+		hi.setVisible(true);
 	}
 
 }
